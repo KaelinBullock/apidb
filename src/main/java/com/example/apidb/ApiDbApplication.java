5270c6f9e94c28d3141b17fafb3471f36f1b9c5f
Kaelin <kaelin.bullock@gmail.com>
2022-08-04 14:05:35 +0700

    db

---
+++ src/main/java/com/example/apidb/ApiDbApplication.java
@@ -0,0 +1,39 @@
+package com.example.apidb;
+
+import com.example.apidb.user.User;
+import com.example.apidb.user.UserService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@SpringBootApplication
+public class ApiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDbApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserService userService){
+		return args -> {
+			// read JSON and load json
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<User>> typeReference = new TypeReference<>() {};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/users.json");
+			try {
+				List<User> users = mapper.readValue(inputStream, typeReference);
+				userService.save(users);
+				System.out.println("Users Saved!");
+			} catch (IOException e){
+				System.out.println("Unable to save users: " + e.getMessage());
+			}
+		};
+	}
+}
---
+++ src/main/java/com/example/apidb/user/UserService.java
@@ -0,0 +1,26 @@
+package com.example.apidb.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Iterable<User> list() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+    public Iterable<User> save(List<User> users) {
+        return userRepository.saveAll(users);
+    }
+}
